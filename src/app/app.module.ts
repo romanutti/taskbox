295045f946e7c8540130576f93e838b3eb17161c
Marco Romanutti <m.romanutti@gmail.com>
2021-11-11 21:23:34 +0100

    add inbox screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { TaskModule } from './components/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-
 import { AppComponent } from './app.component';
 
+import { InboxScreenComponent } from './components/inbox-screen.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InboxScreenComponent, PureInboxScreenComponent],
   imports: [
     BrowserModule,
     TaskModule,
